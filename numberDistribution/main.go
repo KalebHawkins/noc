66a9d33ca97a2e53d7eac6dbe422bc6b3e4f4fdc
Kaleb Hawkins <KalebHawkins@outlook.com>
2023-01-09 21:05:58 -0600

    add normal distribution example

--- numberDistribution/main.go
+++ numberDistribution/main.go
@@ -14,6 +14,13 @@ const (
 	scrHeight = 480
 )
 
+type State int
+
+const (
+	Random State = iota
+	Normal
+)
+
 type Bar struct {
 	x, y, w, h float64
 }
@@ -27,10 +34,29 @@ func (b *Bar) Update() {
 	b.h += r
 }
 
+type Circle struct {
+	x, y, r float64
+	color   color.Color
+}
+
+func (c *Circle) Draw(dst *ebiten.Image) {
+	ebitenutil.DrawCircle(dst, c.x, c.y, c.r, c.color)
+}
+
+func (c *Circle) Update() {
+	r := rand.NormFloat64()
+	stdDev := 60.0
+	mean := 320.0
+
+	c.x = stdDev*r + mean
+}
+
 type Game struct {
 	// 640 (scrWidth) / 32 (barWidth) = 20 bars.
 	bars       [20]*Bar
 	background *ebiten.Image
+	State
+	*Circle
 }
 
 func generateBars() [20]*Bar {
@@ -49,32 +75,62 @@ func generateBars() [20]*Bar {
 func NewGame() *Game {
 
 	return &Game{
-		background: ebiten.NewImage(scrWidth, scrHeight),
 		bars:       generateBars(),
+		background: ebiten.NewImage(scrWidth, scrHeight),
+		State:      0,
+		Circle: &Circle{
+			x:     scrWidth / 2,
+			y:     scrHeight / 2,
+			r:     10,
+			color: color.RGBA{255, 255, 255, 25},
+		},
 	}
 }
 
 func (g *Game) Update() error {
-	for _, b := range g.bars {
-		b.Update()
+	if g.State == Random {
+		for _, b := range g.bars {
+			b.Update()
+		}
+	}
+
+	if g.State == Normal {
+		g.Circle.Update()
 	}
 
 	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
-		g.bars = generateBars()
+		g.background.Fill(color.Black)
+		g.State++
+		if g.State > 1 {
+			g.State = 0
+		}
 	}
 	return nil
 }
 
 func (g *Game) Draw(scr *ebiten.Image) {
-	g.background.Fill(color.Black)
 
-	for _, b := range g.bars {
-		b.Draw(g.background)
+	if g.State == Random {
+		g.background.Fill(color.Black)
+		for _, b := range g.bars {
+			b.Draw(g.background)
+		}
+	}
+
+	if g.State == Normal {
+		g.Circle.Draw(g.background)
 	}
 
 	scr.DrawImage(g.background, nil)
 
-	ebitenutil.DebugPrint(scr, "Click to restart the animation.\n\nExample: Normal (Uniform) Number Distribution")
+	ebitenutil.DebugPrint(scr, "Click to swap the animation.")
+
+	switch g.State {
+	case Random:
+		ebitenutil.DebugPrintAt(scr, "Example: Random Distribution", 0, 20)
+	case Normal:
+		ebitenutil.DebugPrintAt(scr, "Example: Normal Distribution", 0, 20)
+	}
 }
 
 func (g *Game) Layout(outWidth, outHeight int) (int, int) {
